67c5a083310a933388e80b881951eddd6cd794df
Mbah Neilvani <neilvanimbah@gmail.com>
2023-06-07 11:43:35 -0700

    added some features

---
+++ Room.js
@@ -0,0 +1,15 @@
+// const bannerInfo=[{
+//     imgSrc:'./img1',
+//     title:'Discover innovative ways to decorate',
+//     text:'We provide unmatched quality, comfort, and style for property owners across the country.'
+// },{
+//     imgSrc:'./img2',
+//     title:'Discover innovative ways to decorate',
+//     text:'We provide unmatched quality, comfort, and style for property owners across the country.'
+// },
+// {
+//     imgSrc:'./img3',
+//     title:'Discover innovative ways to decorate',
+//     text:'We provide unmatched quality, comfort, and style for property owners across the country.'
+// }
+// ]

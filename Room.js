1123d87ad2e1bf164994e7f78e429d3f1db890d7
Mbah Neilvani <neilvanimbah@gmail.com>
2024-01-30 14:14:45 +0100

    new update

--- Room.js
+++ Room.js
@@ -13,3 +13,22 @@
 //     text:'We provide unmatched quality, comfort, and style for property owners across the country.'
 // }
 // ]
+const headerInfo=[
+    {src:'/img/desktop-image-hero-1.jpg', content:`We provide unmatched quality, comfort, and style for property
+    owners across the country. Our experts combine form and function
+    in bringing your vision to life. Create a room in your own style
+    with our collection and make your property a reflection of you and
+    what you love.`},
+    {src:'img/room.jpg', content:'yyiwjhwusujd'},
+    {src:'img/room.jpg', content:'yyiwjhwusujd'},
+];
+
+const button=document.getElementById('btn-fwrd');
+const img=document.getElementById('img');
+const content=document.getElementById('content');
+
+button.onclick(()=>{
+    const currentIndex=img.data-index;
+    img.src=headerInfo[currentIndex>=2?0:currentIndex+1];
+    content.innerHTML=headerInfo[currentIndex>=2?0:currentIndex+1];
+})

6afa0e51ed8cb33c5b85811507312a06f2d7c56f
Mbah Neilvani <neilvanimbah@gmail.com>
2024-03-28 12:30:54 +0100

    made some css and html changes

--- Room.js
+++ Room.js
@@ -1,3 +1,68 @@
+document.getElementById('left').addEventListener('click', changeImage)
+
+const imgOne = document.getElementsByClassName('img').item
+
+function changeImage() {
+    let imgSrc = imgOne 
+        title= 'We are available all across the globe',
+        text= 'With stores all over the world, \it\'s easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? \Don\'t hesitate to contact us today.'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById('left').addEventListener('click', left)
+
+function left(){
+    
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // const bannerInfo=[{
 //     imgSrc:'./img1',
 //     title:'Discover innovative ways to decorate',
@@ -13,22 +78,24 @@
 //     text:'We provide unmatched quality, comfort, and style for property owners across the country.'
 // }
 // ]
-const headerInfo=[
-    {src:'/img/desktop-image-hero-1.jpg', content:`We provide unmatched quality, comfort, and style for property
-    owners across the country. Our experts combine form and function
-    in bringing your vision to life. Create a room in your own style
-    with our collection and make your property a reflection of you and
-    what you love.`},
-    {src:'./img/desktop-image-hero-2.jpg', content:'yyiwjhwusujd'},
-    {src:'img/room.jpg', content:'yyiwjhwusujd'},
-];
 
-const button=document.getElementById('btn-fwrd');
-const img=document.getElementById('img');
-const content=document.getElementById('content');
 
-button.onclick(()=>{
-    const currentIndex=img.data-index;
-    img.src=headerInfo[currentIndex>=2?0:currentIndex+1];
-    content.innerHTML=headerInfo[currentIndex>=2?0:currentIndex+1];
-})
+// const headerInfo=[
+//     {src:'/img/desktop-image-hero-1.jpg', content:`We provide unmatched quality, comfort, and style for property
+//     owners across the country. Our experts combine form and function
+//     in bringing your vision to life. Create a room in your own style
+//     with our collection and make your property a reflection of you and
+//     what you love.`},
+//     {src:'./img/desktop-image-hero-2.jpg', content:'yyiwjhwusujd'},
+//     {src:'img/room.jpg', content:'yyiwjhwusujd'},
+// ];
+
+// const button=document.getElementById('btn-fwrd');
+// const img=document.getElementById('img');
+// const content=document.getElementById('content');
+
+// button.onclick(()=>{
+//     const currentIndex=img.data-index;
+//     img.src=headerInfo[currentIndex>=2?0:currentIndex+1];
+//     content.innerHTML=headerInfo[currentIndex>=2?0:currentIndex+1];
+// })

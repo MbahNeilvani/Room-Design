c69a761f227047595677cd06ba44d623ee839b6a
Mbah Neilvani <neilvanimbah@gmail.com>
2024-08-26 21:03:07 +0100

    trying to perfect the page so it can slide images

--- Room.js
+++ Room.js
@@ -1,11 +1,8 @@
 document.getElementById('grp-btn').addEventListener('click', slide)
 
-const existing = document.getElementsByClassName('img')
-let new = src()
-
 
 function slide(){
-    // alert('Working')
+    alert('Working')
     
 }
 

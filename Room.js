74fdc6d322d378c808e04174c18cd14f16ca2387
Mbah Neilvani <neilvanimbah@gmail.com>
2024-08-27 00:04:53 +0100

    working now how to loop through images

--- Room.js
+++ Room.js
@@ -1,141 +1,24 @@
+
+const m02 = document.getElementById('img02')
+const m03 = document.getElementById('img03')
+
+let run = [m02, m03]
+
 document.getElementById('grp-btn').addEventListener('click', slide)
 
 
+
 function slide(){
-    alert('Working')
-    
+    // alert('Working')
+    for(let i = 2; i <= run.length; i++){
+        
+        img02.classList.toggle('hidden')
+        img03.classList.add('hidden')
+    }
+     
+    // document.getElementsById('show').innerHTML = run 
 }
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const burgerOpen = document.getElementById('burger')
-// const burgerclose = document.getElementById('burger_close')
-
-// document.getElementById('burger').addEventListener('click', change)
-
-// function change(){
-//     burgerOpen.classList.add('hiddin')
-//     burgerclose.classList.toggle('hiddin')
-//     // console.log('working')
-// }
-
-// change()
-
-// document.getElementById('burger').addEventListener('click', changeburger)
-// function changeburger(){
-//     console.log('working')
-// }
-
-// changeburger()
-
-
-// document.getElementById('left').addEventListener('click', changeImage)
-
-// const imgOne = document.getElementsByClassName('img').item
-
-// function changeImage() {
-//     let imgSrc = imgOne 
-//         title= 'We are available all across the globe',
-//         text= 'With stores all over the world, \it\'s easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? \Don\'t hesitate to contact us today.'
-// }
-
-
-// document.getElementById('left').addEventListener('click', left)
-
-// function left(){
-    
-
-    
-// }
-
-// const bannerInfo=[{
-//     imgSrc:'./img1',
-//     title:'Discover innovative ways to decorate',
-//     text:'We provide unmatched quality, comfort, and style for property owners across the country.'
-// },{
-//     imgSrc:'./img2',
-//     title:'Discover innovative ways to decorate',
-//     text:'We provide unmatched quality, comfort, and style for property owners across the country.'
-// },
-// {
-//     imgSrc:'./img3',
-//     title:'Discover innovative ways to decorate',
-//     text:'We provide unmatched quality, comfort, and style for property owners across the country.'
-// }
-// ]
-
-
-// const headerInfo=[
-//     {src:'/img/desktop-image-hero-1.jpg', content:`We provide unmatched quality, comfort, and style for property
-//     owners across the country. Our experts combine form and function
-//     in bringing your vision to life. Create a room in your own style
-//     with our collection and make your property a reflection of you and
-//     what you love.`},
-//     {src:'./img/desktop-image-hero-2.jpg', content:'yyiwjhwusujd'},
-//     {src:'img/room.jpg', content:'yyiwjhwusujd'},
-// ];
-
-// const button=document.getElementById('btn-fwrd');
-// const img=document.getElementById('img');
-// const content=document.getElementById('content');
-
-// button.onclick(()=>{
-//     const currentIndex=img.data-index;
-//     img.src=headerInfo[currentIndex>=2?0:currentIndex+1];
-//     content.innerHTML=headerInfo[currentIndex>=2?0:currentIndex+1];
-// })
